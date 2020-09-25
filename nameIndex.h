da537734d18bf51d0f36f665aeae799ed54b9697
clark174 <70905278+clark174@users.noreply.github.com>
2020-09-25 10:24:06 -0400

    Create nameIndex.h

---
+++ nameIndex.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include "characters.h"
+using namespace std;
+
+class nameIndex
+{
+  public:
+    nameIndex(string filename);//constructor that creates the index.
+    string lowercase(string str);//simple lowercase function.
+    vector <int> indexSearch(string word);//searches the index for word and returns a vector of references.
+    string getName(string str);//gets the name of a character.
+
+  private:
+    map<string, vector<int> > refs;//maps from character names to an int vector.
+};
