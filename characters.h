dab53b6c07dd5a120ad31c27ee06a4b46199d9c7
clark174 <70905278+clark174@users.noreply.github.com>
2020-09-25 10:22:33 -0400

    Create characters.h

---
+++ characters.h
@@ -0,0 +1,33 @@
+#ifndef CHARACTERS_H
+#define CHARACTERS_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+class characters
+{
+  public:
+    characters(); //default constructor;
+    characters(string aInfo); //assigns the line of the character to info.
+    string formatInfo(string str); //removes special characters from the str and also adds spaces to make it look neater.
+    void getInfo(string str); //assigns each section of info to the corresponding variable below.
+    string info;
+    string name;
+    string identity;
+    string alliance;
+    string eyes;
+    string hair;
+    string gender;
+    string alive;
+    string appearances;
+    string firstAppearance;
+    string firstYear;
+
+
+  private:
+};
+
+#endif
