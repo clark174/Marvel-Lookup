36551b4be7074f66b5d46fed6d7d06535f19024c
clark174 <70905278+clark174@users.noreply.github.com>
2020-09-25 10:24:47 -0400

    Create nameMap.h

---
+++ nameMap.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include "characters.h"
+
+using namespace std;
+
+class nameMap
+{
+  public:
+    nameMap(string filename);//constructor that fills characterList with the characters.
+    void displayName(int reference);//displays the name of the character.
+    void displayInfo(int reference);//displays the info of the character.
+
+  private:
+    vector <characters> characterList;//vector to store the characters.
+};
